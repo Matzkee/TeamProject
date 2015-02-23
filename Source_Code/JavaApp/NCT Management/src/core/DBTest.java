9ab6044e3b0cd7b658b5b3d9dab16f5c299121ec
Matthew <pietraszewski.mateusz@gmail.com>
2015-02-23 17:06:02 +0000

    Setting up working directory & sample DB code

---
+++ Source_Code/JavaApp/NCT Management/src/core/DBTest.java
@@ -0,0 +1,59 @@
+package core;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main( String args[] )
+	{
+		try{
+			// Load the database driver
+			Class.forName( "com.mysql.jdbc.Driver" ) ;
+
+			// Get a connection to the database
+			Connection conn = DriverManager.getConnection( "jdbc:mysql://83.212.127.2:3306/NCT", "user", "TeamGravity123" ) ;
+
+
+
+			// Get a statement from the connection
+			Statement stmt = conn.createStatement() ;
+			
+			//Sample Delete Statement
+			//stmt.executeUpdate("DELETE FROM Owner WHERE Owner_Id IN (222222);");
+			
+			// Execute the query
+			ResultSet rs = stmt.executeQuery( "SELECT * FROM Owner" ) ;
+
+			// Loop through the result set
+			while( rs.next() )
+				System.out.println( rs.getString(1) + "\t" + rs.getString(2) + "\t" + rs.getString(3) + "\t" + rs.getString(4)) ;
+
+			// Close the result set, statement and the connection
+			rs.close() ;
+			stmt.close() ;
+			conn.close() ;
+		}
+		catch( SQLException se ){
+			System.out.println( "SQL Exception:" ) ;
+
+			// Loop through the SQL Exceptions
+			while( se != null ){
+				System.out.println( "State  : " + se.getSQLState()  ) ;
+				System.out.println( "Message: " + se.getMessage()   ) ;
+				System.out.println( "Error  : " + se.getErrorCode() ) ;
+
+				se = se.getNextException() ;
+			}
+		}
+		catch( Exception e ){
+			System.out.println( e ) ;
+		}
+	}
+}
