6db0f488febf795a6fe03c74cff38397eac5590b
Matthew <pietraszewski.mateusz@gmail.com>
2015-03-23 20:33:04 +0000

    Testing new menu background

---
+++ Source_Code/JavaApp/NCT Management/src/frames/AdminMenu_v2.java
@@ -0,0 +1,47 @@
+package frames;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+public class AdminMenu_v2 extends JFrame {
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AdminMenu_v2 frame = new AdminMenu_v2();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AdminMenu_v2() {
+		setSize(815,590);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		contentPane = new JPanel();
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel background = new JLabel("");
+		background.setIcon(new ImageIcon(AdminMenu_v2.class.getResource("/graphics/AdminMenu_v2.jpg")));
+		background.setBounds(0, 0, 800, 550);
+		contentPane.add(background);
+	}
+}
