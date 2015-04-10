97fc8becbd5a653ca6d15c759a3971cc1ed2fc57
Matthew <pietraszewski.mateusz@gmail.com>
2015-04-10 21:09:15 +0100

    Initializing Mechanic Frame

---
+++ Source_Code/JavaApp/NCT Management/src/frames/MechanicBookingsTable.java
@@ -0,0 +1,240 @@
+package frames;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.border.EmptyBorder;
+
+import components.TableModel;
+import components.TableMouseListener;
+import components.TableRenderer;
+
+import core.Booking;
+import core.Client;
+
+public class MechanicBookingsTable extends JPanel implements MouseListener{
+	/**
+	 * Variable declaration.
+	 */
+	private static final long serialVersionUID = 1L;
+	private int garageId = 1;
+	private ArrayList<Booking> allBookings = new ArrayList<>();
+	private Client mainClient;
+	private JTextField txtCarReg, txtDate, txtTime;
+	private JLabel lblCarRegistration, lblDate, lblTime;
+	private JLabel systemInfo;
+	private TableModel tableModel;
+	private JTable table;
+	private JScrollPane tableScrollPane;
+	private BookingCreationPane newBookingPane;
+	private boolean isCarRegEditToggled, isDateEditToggled, isTimeEditToggled;
+	/* Text Font */
+	private final Font TEXTFONT = new Font("Segoe UI", Font.PLAIN, 13);
+
+	/**
+	 * Create the panel.
+	 * Constructor
+	 */
+	public MechanicBookingsTable(int garage, Client programClient, JLabel sysInfo) {
+		garageId = garage;
+		systemInfo = sysInfo;
+		// Main Panel Settings
+		setSize(800,400);
+		setLayout(null);
+		setOpaque(false);
+		addMouseListener(this);
+		
+		isCarRegEditToggled = false;
+		isDateEditToggled = false;
+		isTimeEditToggled = false;
+		
+		mainClient = programClient;
+		mainClient.viewBookings();
+		
+		allBookings = mainClient.getBookings();
+		
+		showTable();
+		showLabels();
+		showTextFields();
+		showButtons();
+		
+		// Assign mouse listener to table
+		// Set at the end for other components to be initialised first
+		table.addMouseListener(new TableMouseListener(table, txtCarReg, txtDate, txtTime, systemInfo));
+	}
+	
+	public void showTable(){
+		// Setting up the table
+		tableModel = new TableModel(allBookings);
+		table = new JTable(tableModel);
+		table.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		table.setBorder(null);
+		table.setShowGrid(false);
+		table.setOpaque(false);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setDefaultRenderer(Booking.class, new TableRenderer());
+		table.setRowHeight(40);
+		table.setTableHeader(null);
+		// ScrollPane for the table
+		tableScrollPane = new JScrollPane(table);
+		tableScrollPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+		tableScrollPane.getViewport().setOpaque(false);
+		tableScrollPane.setOpaque(false);
+		tableScrollPane.setBounds(390, 25, 400, 300);
+		add(tableScrollPane);
+	}
+	
+	public void showLabels(){
+		// CarReg label
+		lblCarRegistration = new JLabel("Car Registration: ");
+		lblCarRegistration.setFont(TEXTFONT);
+		lblCarRegistration.setForeground(Color.LIGHT_GRAY);
+		lblCarRegistration.setBounds(10, 70, 110, 20);
+		add(lblCarRegistration);
+		// Date label
+		lblDate = new JLabel("Date: ");
+		lblDate.setFont(TEXTFONT);
+		lblDate.setForeground(Color.LIGHT_GRAY);
+		lblDate.setBounds(10, 100, 110, 20);
+		add(lblDate);
+		// Time label
+		lblTime = new JLabel("Time: ");
+		lblTime.setFont(TEXTFONT);
+		lblTime.setForeground(Color.LIGHT_GRAY);
+		lblTime.setBounds(10, 130, 110, 20);
+		add(lblTime);
+	}
+	
+	public void showTextFields(){
+		// CarReg text field
+		txtCarReg = new JTextField();
+		txtCarReg.setBorder(null);
+		txtCarReg.setOpaque(false);
+		txtCarReg.setEditable(false);
+		txtCarReg.setForeground(Color.LIGHT_GRAY);
+		txtCarReg.setFont(TEXTFONT);
+		txtCarReg.setBounds(130, 70, 100, 20);
+		add(txtCarReg);
+		txtCarReg.setColumns(10);
+		// Date text field
+		txtDate = new JTextField();
+		txtDate.setBorder(null);
+		txtDate.setOpaque(false);
+		txtDate.setForeground(Color.LIGHT_GRAY);
+		txtDate.setFont(TEXTFONT);
+		txtDate.setEditable(false);
+		txtDate.setBounds(130, 100, 100, 20);
+		add(txtDate);
+		txtDate.setColumns(10);
+		// Time text field
+		txtTime = new JTextField();
+		txtTime.setBorder(null);
+		txtTime.setOpaque(false);
+		txtTime.setForeground(Color.LIGHT_GRAY);
+		txtTime.setFont(TEXTFONT);
+		txtTime.setEditable(false);
+		txtTime.setBounds(130, 130, 100, 20);
+		add(txtTime);
+		txtTime.setColumns(10);
+	}
+	
+	public void showButtons(){
+	}
+	public void toggleFields(boolean carReg, boolean date, boolean time){
+		txtCarReg.setEditable(carReg);
+		txtDate.setEditable(date);
+		txtTime.setEditable(time);
+	}
+	public void toggleFieldPaints(boolean carReg, boolean date, boolean time){
+		isCarRegEditToggled = carReg;
+		isDateEditToggled = date;
+		isTimeEditToggled = time;
+	}
+	public void clearFields(){
+		txtCarReg.setText("");
+		txtDate.setText("");
+		txtTime.setText("");
+	}
+	public void clearAllSelections(){
+		toggleFields(false, false, false);
+		toggleFieldPaints(false, false, false);
+		clearFields();
+	};
+	
+	/*
+	 * Custom gradient paint for JText boxes
+	 */
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g;
+		GradientPaint gp, gp2, gp3;
+		gp = new GradientPaint(txtCarReg.getX()+(txtCarReg.getWidth()/2),txtCarReg.getY()+txtCarReg.getHeight(),new Color(255,255,255,50),
+				txtCarReg.getX()+(txtCarReg.getWidth()/2),txtCarReg.getY()+(txtCarReg.getHeight()/4),new Color(0,0,0,0));
+		gp2 = new GradientPaint(txtDate.getX()+(txtDate.getWidth()/2),txtDate.getY()+txtDate.getHeight(),new Color(255,255,255,50),
+				txtDate.getX()+(txtDate.getWidth()/2),txtDate.getY()+(txtDate.getHeight()/4),new Color(0,0,0,0));
+		gp3 = new GradientPaint(txtTime.getX()+(txtTime.getWidth()/2),txtTime.getY()+txtTime.getHeight(),new Color(255,255,255,50),
+				txtTime.getX()+(txtTime.getWidth()/2),txtTime.getY()+(txtTime.getHeight()/4),new Color(0,0,0,0));
+		if(isCarRegEditToggled){
+			g2d.setPaint(gp);
+			g2d.fillRect(txtCarReg.getX(), txtCarReg.getY(), txtCarReg.getWidth(), txtCarReg.getHeight());
+		}
+		else if(isDateEditToggled){
+			g2d.setPaint(gp2);
+			g2d.fillRect(txtDate.getX(), txtDate.getY(), txtDate.getWidth(), txtDate.getHeight());			
+		}
+		else if(isTimeEditToggled){
+			g2d.setPaint(gp3);
+			g2d.fillRect(txtTime.getX(), txtTime.getY(), txtTime.getWidth(), txtTime.getHeight());			
+		}
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		Object o = e.getSource();
+		if(o.equals(this)){
+			// Double click event
+			// Clear systemInfo text
+			if(e.getClickCount() == 2 && !e.isConsumed()){
+				e.consume();
+				newBookingPane.setVisible(false);
+				systemInfo.setText("");
+			}
+		}
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		Object o = e.getSource();
+		// Case: JPanel, main booking panel
+		if(o.equals(this)){
+			systemInfo.setText("Main Panel");
+			if (table.getSelectedRow() != -1){
+				table.clearSelection();
+			}
+		}
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+}
---
+++ Source_Code/JavaApp/NCT Management/src/frames/MechanicMenu.java
@@ -0,0 +1,199 @@
+package frames;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+import core.Client;
+
+public class MechanicMenu extends JFrame implements MouseListener{
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Variable declaration.
+	 */
+	private Login loginPage;
+	private JPanel contentPane;
+	private JLabel lblHome;
+	private JLabel lblBookings;
+	private JLabel lblLogOut;
+	private int garageId;
+	private Client mainClient;
+	private final Color HIGHLIGHTCOLOR = new Color(214,137,4);
+	private final Color HOVERCOLOR = Color.LIGHT_GRAY;
+	private final Color AMBIENTCOLOR = Color.GRAY;
+	private final Font TEXTFONT = new Font("Segoe UI", Font.PLAIN, 20);
+	private final Font SMALLTEXTFONT = new Font("Segoe UI", Font.PLAIN, 10);
+	private BookingsTable bookingPane;
+	private ResultPane resultPane;
+	private JLabel systemInfo;
+
+
+	/**
+	 * Create the frame.
+	 */
+	public MechanicMenu(int garage, Client programClient, Login loginP) {
+		garageId = garage;
+		mainClient = programClient;
+		loginPage = loginP;
+		// Frame Settings
+		setTitle("NCT Management");
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Content Panel
+		contentPane = new JPanel();
+		setContentPane(contentPane);
+		contentPane.setPreferredSize(new Dimension(800, 550));
+		contentPane.setLayout(null);
+		
+		lblHome = new JLabel("HOME", SwingConstants.CENTER);
+		lblHome.setFont(TEXTFONT);
+		lblHome.setForeground(AMBIENTCOLOR);
+		lblHome.setBounds(0, 0, 100, 30);
+		lblHome.addMouseListener(this);
+		contentPane.add(lblHome);
+		
+		lblBookings = new JLabel("BOOKINGS", SwingConstants.CENTER);
+		lblBookings.setForeground(AMBIENTCOLOR);
+		lblBookings.setFont(TEXTFONT);
+		lblBookings.setBounds(130, 0, 100, 30);
+		lblBookings.addMouseListener(this);
+		contentPane.add(lblBookings);
+		
+		lblLogOut = new JLabel("LOG OUT", SwingConstants.CENTER);
+		lblLogOut.setForeground(AMBIENTCOLOR);
+		lblLogOut.setFont(TEXTFONT);
+		lblLogOut.setBounds(390, 0, 100, 30);
+		lblLogOut.addMouseListener(this);
+		contentPane.add(lblLogOut);
+		
+		// System Label
+		// Any relevant & useful info 
+		systemInfo = new JLabel("", SwingConstants.RIGHT);
+		systemInfo.setBounds(540, 480, 250, 12);
+		systemInfo.setFont(SMALLTEXTFONT);
+		systemInfo.setForeground(HOVERCOLOR);
+		contentPane.add(systemInfo);
+		
+		bookingPane = new BookingsTable(garageId, mainClient, systemInfo);
+		bookingPane.setBounds(0, 50, 800, 400);
+		contentPane.add(bookingPane);
+		bookingPane.setVisible(false);
+		
+		JLabel background = new JLabel("");
+		background.setIcon(new ImageIcon(AdminMenu.class.getResource("/graphics/AdminMenu.jpg")));
+		background.setBounds(0, 0, 800, 550);
+		contentPane.add(background);
+		
+		pack();
+	}
+	
+	
+	/**
+	 * Launch the application.
+	 * Placeholder:
+	 * remove once program is finished
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MechanicMenu frame = new MechanicMenu(1, new Client(), new Login());
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+
+	// Mouse Listener Methods
+	// Interface methods
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		JLabel button = (JLabel) e.getSource();
+		switch(button.getText()){
+			case "HOME":
+				bookingPane.setVisible(false);
+				resultPane.setVisible(false);
+				button.setForeground(HIGHLIGHTCOLOR);
+				systemInfo.setText("HOME");
+			break;
+			case "BOOKINGS":
+				resultPane.setVisible(false);
+				bookingPane.setVisible(true);
+				button.setForeground(HIGHLIGHTCOLOR);
+				systemInfo.setText("BOOKINGS");
+			break;
+			case "LOG OUT":
+				bookingPane.setVisible(false);
+				resultPane.setVisible(false);
+				button.setForeground(HIGHLIGHTCOLOR);
+				systemInfo.setText("LOG OUT");
+				mainClient.logOut();
+				loginPage.setVisible(true);
+				dispose();
+			break;
+		}
+		System.out.println("Clicked: "+button.getText());
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		JLabel button = (JLabel) e.getSource();
+		switch(button.getText()){
+			case "HOME":
+				button.setForeground(AMBIENTCOLOR);
+			break;
+			case "BOOKINGS":
+				button.setForeground(AMBIENTCOLOR);
+			break;
+			case "LOG OUT":
+				button.setForeground(AMBIENTCOLOR);
+			break;
+		}
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		JLabel button = (JLabel) e.getSource();
+		switch(button.getText()){
+			case "HOME":
+				button.setForeground(HOVERCOLOR);
+			break;
+			case "BOOKINGS":
+				button.setForeground(HOVERCOLOR);
+			break;
+			case "LOG OUT":
+				button.setForeground(HOVERCOLOR);
+			break;
+		}
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		JLabel button = (JLabel) e.getSource();
+		switch(button.getText()){
+			case "HOME":
+				button.setForeground(AMBIENTCOLOR);
+			break;
+			case "BOOKINGS":
+				button.setForeground(AMBIENTCOLOR);
+			break;
+			case "LOG OUT":
+				button.setForeground(AMBIENTCOLOR);
+			break;
+		}
+	}
+}
